e4cba31ec995be002210919c5bebf1c4508424f9
Colm Ó hIcí <colmohici@gmail.com>
2018-11-17 09:35:25 +0000

    use inkwell

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-extern crate dreammaker;
-extern crate llvm_sys_wrapper;
 extern crate lalrpop_util;
+extern crate inkwell;
 
-use llvm_sys_wrapper as llvm;
-use llvm_sys_wrapper::LLVMFunctionType; // ugh
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
@@ -14,7 +11,7 @@ lalrpop_mod!(pub basic);
 mod ast;
 
 fn main() {
-    llvm::LLVM::initialize();
+    inkwell::targets::Target::initialize_native(&inkwell::targets::InitializationConfig::default()).unwrap();
 
     let mut file = File::open("exp.bsc").unwrap();
     let mut buf = String::new();
@@ -36,14 +33,10 @@ fn main() {
 fn build_statements(root: Vec<ast::Statement>) -> Proc {
     let mut proc = Proc::new();
 
-    let mut local_id = 0u32;
-
     // finddecls
     for stmt in root.iter() {
         if let ast::Statement::Decl(v) = stmt {
-            proc.locals.insert(v.clone(), local_id);
-            proc.local_defs.push(local_id);
-            local_id += 1;
+            proc.add_local(Some(&v));
         }
     }
 
@@ -55,6 +48,31 @@ fn build_statements(root: Vec<ast::Statement>) -> Proc {
 }
 
 impl Proc {
+    fn new() -> Self {
+        Self {
+            locals: Vec::new(),
+            vars: HashMap::new(),
+            blocks: Vec::new(),
+        }
+    }
+
+    fn add_local(&mut self, name: Option<&str>) -> u32 {
+        let id = self.locals.len() as u32;
+
+        let local = Local {
+            id: id,
+            name: name.map(|s| s.to_string()),
+        };
+
+        self.locals.push(local);
+
+        if let Some(var) = name {
+            self.vars.insert(var.into(), id);
+        }
+
+        id
+    }
+
     fn build_block(&mut self, stmts: &[ast::Statement]) -> Block {
         let mut block = Block::new();
 
@@ -84,10 +102,9 @@ impl Proc {
             ast::Expr::Add(lhs, rhs) => {
                 let lhs_expr = self.build_expr(lhs, block);
                 let rhs_expr = self.build_expr(rhs, block);
-                let res_id = self.local_defs.len() as u32;
-                self.local_defs.push(res_id);
-                block.ops.push(Op::Add(res_id, lhs_expr, rhs_expr));
-                Expr::Place(Place::Local(res_id))
+                let local = self.add_local(None);
+                block.ops.push(Op::Add(local, lhs_expr, rhs_expr));
+                Expr::Place(Place::Local(local))
             }
         }
     }
@@ -103,29 +120,27 @@ impl Proc {
     }
 
     fn lookup_var(&self, var: &str) -> Option<u32> {
-        self.locals.get(var).map(|id| *id)
+        self.vars.get(var).map(|id| *id)
     }
 }
 
 type LocalID = u32;
 
 #[derive(Debug)]
-struct Proc {
-    locals: HashMap<String, u32>,
+struct Local {
+    id: u32,
+    name: Option<String>,
+}
 
-    local_defs: Vec<u32>,
+#[derive(Debug)]
+struct Proc {
+    locals: Vec<Local>,
+    vars: HashMap<String, u32>,
 
     blocks: Vec<Block>,
 }
 
 impl Proc {
-    fn new() -> Self {
-        Self {
-            locals: HashMap::new(),
-            local_defs: Vec::new(),
-            blocks: Vec::new(),
-        }
-    }
 }
 
 #[derive(Debug)]
@@ -182,13 +197,13 @@ enum Terminator {
 
 #[derive(Debug)]
 struct ProcEmit {
-    local_allocs: Vec<llvm::LLVMValueRef>,
-    blocks: Vec<llvm::LLVMBasicBlockRef>,
-    func: llvm::Function,
+    local_allocs: Vec<inkwell::values::PointerValue>,
+    blocks: Vec<inkwell::basic_block::BasicBlock>,
+    func: inkwell::values::FunctionValue,
 }
 
 impl ProcEmit {
-    fn new(func: llvm::Function) -> Self {
+    fn new(func: inkwell::values::FunctionValue) -> Self {
         Self {
             local_allocs: Vec::new(),
             blocks: Vec::new(),
@@ -199,14 +214,14 @@ impl ProcEmit {
 
 #[derive(Debug)]
 struct Builder {
-    ctx: llvm::Context,
-    builder: llvm::Builder,
-    module: llvm::Module,
+    ctx: inkwell::context::Context,
+    builder: inkwell::builder::Builder,
+    module: inkwell::module::Module,
 }
 
 impl Builder {
     fn new() -> Self {
-        let ctx = llvm::Context::global_context();
+        let ctx = inkwell::context::Context::create();
         Self {
             builder: ctx.create_builder(),
             module: ctx.create_module("main"),
@@ -214,26 +229,30 @@ impl Builder {
         }
     }
 
-    fn emit_entry_block(&self, proc: &Proc, proc_emit: &mut ProcEmit) -> llvm::LLVMBasicBlockRef {
+    fn emit_entry_block(&self, proc: &Proc, proc_emit: &mut ProcEmit) -> inkwell::basic_block::BasicBlock {
         let block = proc_emit.func.append_basic_block("entry");
-        self.builder.position_at_end(block);
+        self.builder.position_at_end(&block);
 
-        for id in proc.local_defs.iter() {
-            let alloc = self.builder.build_alloca(self.ctx.FloatType());
+        for local in proc.locals.iter() {
+            let name = match local.name {
+                Some(ref s) => s,
+                None => "",
+            };
+            let alloc = self.builder.build_alloca(self.ctx.f32_type(), name);
             proc_emit.local_allocs.push(alloc);
         }
 
         block
     }
 
-    fn finalize_entry_block(&self, entry: llvm::LLVMBasicBlockRef, start: llvm::LLVMBasicBlockRef) {
-        self.builder.position_at_end(entry);
-        self.builder.build_br(start);
+    fn finalize_entry_block(&self, entry: &inkwell::basic_block::BasicBlock, start: &inkwell::basic_block::BasicBlock) {
+        self.builder.position_at_end(&entry);
+        self.builder.build_unconditional_branch(&start);
     }
 
     fn emit_proc(&self, proc: &Proc) {
-        let func_type = llvm::fn_type!(self.ctx.VoidType());
-        let func = self.module.add_function("main", func_type);
+        let func_type = self.ctx.void_type().fn_type(&[], false);
+        let func = self.module.add_function("main", func_type, None);
         let mut proc_emit = ProcEmit::new(func);
         let entry_block = self.emit_entry_block(proc, &mut proc_emit);
 
@@ -243,33 +262,37 @@ impl Builder {
         }
 
         for (id, block) in proc.blocks.iter().enumerate() {
-            let ll_block = proc_emit.blocks[id];
+            let ll_block = &proc_emit.blocks[id];
             self.builder.position_at_end(ll_block);
             self.emit_block(block, &proc_emit);
         }
 
-        self.finalize_entry_block(entry_block, proc_emit.blocks[0]);
+        self.finalize_entry_block(&entry_block, &proc_emit.blocks[0]);
 
-        self.module.dump();
+        self.module.print_to_stderr();
 
-        let engine = llvm::Engine::create_jit_engine(self.module.as_ref()).unwrap();;
-        engine.run_function(proc_emit.func.as_ref(), &mut []);
-    }
+        let engine = self.module.create_jit_execution_engine(inkwell::OptimizationLevel::None).unwrap();
 
-    fn lit_to_val(&self, lit: &Literal) -> llvm::LLVMValueRef {
-        match lit {
-            Literal::Num(x) => self.ctx.Float(*x as f64),
-            _ => self.ctx.Float(0f64),
+        unsafe {
+            let func = engine.get_function::<unsafe extern "C" fn()>("main").unwrap();
+            func.call();
         }
     }
 
-    fn load_expr(&self, expr: &Expr, proc_emit: &ProcEmit) -> llvm::LLVMValueRef {
+    fn lit_to_val(&self, lit: &Literal) -> inkwell::values::BasicValueEnum {
+        match lit {
+            Literal::Num(x) => self.ctx.f32_type().const_float(*x as f64).into(),
+            _ => self.ctx.f32_type().const_float(0f64).into(),
+        }
+    }
+
+    fn load_expr(&self, expr: &Expr, proc_emit: &ProcEmit) -> inkwell::values::BasicValueEnum {
         match expr {
-            Expr::Literal(lit) => self.lit_to_val(lit),
+            Expr::Literal(lit) => self.lit_to_val(lit).into(),
             Expr::Place(place) => {
                 match place {
                     Place::Local(id) => {
-                        self.builder.build_load(proc_emit.local_allocs[*id as usize])
+                        self.builder.build_load(proc_emit.local_allocs[*id as usize], "").into()
                     },
                     Place::Global(_) => panic!("todo"),
                 }
@@ -282,25 +305,26 @@ impl Builder {
             match op {
                 Op::Mov(id, expr) => {
                     let val = self.load_expr(expr, proc_emit);
-                    self.builder.build_store(val, proc_emit.local_allocs[*id as usize]);
+                    self.builder.build_store(proc_emit.local_allocs[*id as usize], val);
                 },
 
                 Op::Put(id) => {
-                    let print_type = llvm::fn_type!(self.ctx.VoidType(), self.ctx.FloatType());
-                    let print_func = self.module.get_or_add_function("dm_print_num", print_type);
-                    let val = self.builder.build_load(proc_emit.local_allocs[*id as usize]);
-                    self.builder.build_call(print_func.as_ref(), &mut [val]);
+                    let print_type = self.ctx.void_type().fn_type(&[self.ctx.f32_type().into()], false);
+                    //let print_func = self.module.get_or_add_function("dm_print_num", print_type);
+                    let print_func = self.module.add_function("dm_print_num", print_type, None);
+                    let val = self.builder.build_load(proc_emit.local_allocs[*id as usize], "put");
+                    self.builder.build_call(print_func, &mut [val], "put");
                 },
                 Op::Add(id, lhs, rhs) => {
-                    let lhs_ref = self.load_expr(lhs, proc_emit);
-                    let rhs_ref = self.load_expr(rhs, proc_emit);
-                    let val = self.builder.build_fadd(lhs_ref, rhs_ref);
-                    self.builder.build_store(val, proc_emit.local_allocs[*id as usize]);
+                    let lhs_ref = self.load_expr(lhs, proc_emit).into_float_value();
+                    let rhs_ref = self.load_expr(rhs, proc_emit).into_float_value();
+                    let val = self.builder.build_float_add(lhs_ref, rhs_ref, "sum");
+                    self.builder.build_store(proc_emit.local_allocs[*id as usize], val);
                 }
             }
         }
 
-        self.builder.build_ret_void();
+        self.builder.build_return(None);
     }
 }
 
