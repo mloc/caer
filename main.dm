9dbb37e38d9a03f447ee771c6744b8d61f72fc16
Colm Ó hIcí <colmohici@gmail.com>
2018-12-30 03:28:12 +0000

    dm and indexvecs, oh my

---
+++ main.dm
@@ -0,0 +1,5 @@
+/proc/main()
+	var/x = 7
+	var/y = 3
+	y = x + x + 4 + y
+	world << y + 4
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,23 @@
-extern crate lalrpop_util;
-extern crate inkwell;
-
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
 
-use lalrpop_util::lalrpop_mod;
-lalrpop_mod!(pub basic);
-
-mod ast;
+use dreammaker::ast;
+use indexed_vec::{IndexVec, newtype_index, Idx};
 
 fn main() {
     inkwell::targets::Target::initialize_native(&inkwell::targets::InitializationConfig::default()).unwrap();
 
-    let mut file = File::open("exp.bsc").unwrap();
-    let mut buf = String::new();
-    file.read_to_string(&mut buf).unwrap();
+    let dm_context = dreammaker::Context::default();
+    let preproc = dreammaker::preprocessor::Preprocessor::new(&dm_context, "main.dm".into()).unwrap();
+    let indents = dreammaker::indents::IndentProcessor::new(&dm_context, preproc);
+    let mut parser = dreammaker::parser::Parser::new(&dm_context, indents);
+    parser.enable_procs();
 
-    let parser = basic::StatementsParser::new();
+    let tree = parser.parse_object_tree();
+    let main_proc = &tree.root().get().procs["main"].value[0];
 
-    let res = parser.parse(&buf);
-    println!("{:?}", res);
-    let proc = build_statements(res.unwrap());
+    let proc = build_statements(&main_proc.body);
     println!("{:?}", proc);
 
     let builder = Builder::new();
@@ -30,13 +26,13 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn build_statements(root: Vec<ast::Statement>) -> Proc {
+fn build_statements(root: &Vec<ast::Statement>) -> Proc {
     let mut proc = Proc::new();
 
     // finddecls
     for stmt in root.iter() {
-        if let ast::Statement::Decl(v) = stmt {
-            proc.add_local(Some(&v));
+        if let ast::Statement::Var(v) = stmt {
+            proc.add_local(Some(&v.name));
         }
     }
 
@@ -49,15 +45,17 @@ fn build_statements(root: Vec<ast::Statement>) -> Proc {
 
 impl Proc {
     fn new() -> Self {
-        Self {
-            locals: Vec::new(),
+        let mut new = Self {
+            locals: IndexVec::new(),
             vars: HashMap::new(),
             blocks: Vec::new(),
-        }
+        };
+        new.add_local(None); // return
+        new
     }
 
-    fn add_local(&mut self, name: Option<&str>) -> u32 {
-        let id = self.locals.len() as u32;
+    fn add_local(&mut self, name: Option<&str>) -> LocalId {
+        let id = LocalId::new(self.locals.len());
 
         let local = Local {
             id: id,
@@ -78,64 +76,115 @@ impl Proc {
 
         for stmt in stmts.iter() {
             match stmt {
-                ast::Statement::Decl(_) => {}, // unused for now, scoping
-
-                ast::Statement::Assignment(var, expr) => {
-                    let var_id = self.lookup_var(var).unwrap();
-                    let asg_expr = self.build_expr(expr, &mut block);
-                    block.ops.push(Op::Mov(var_id, asg_expr));
+                ast::Statement::Var(v) => {
+                    if let Some(ref expr) = v.value {
+                        self.build_assign(&v.name, expr, &mut block);
+                    }
                 },
 
-                ast::Statement::Put(var) => {
-                    let var_id = self.lookup_var(var).unwrap();
-                    block.ops.push(Op::Put(var_id));
+                ast::Statement::Expr(expr) => {
+                    match expr {
+                        ast::Expression::AssignOp { op, lhs, rhs } => {
+                            let var = match lhs.as_ref() {
+                                ast::Expression::Base { unary, term, follow } => {
+                                    match term {
+                                        ast::Term::Ident(ref s) => s,
+                                        _ => unimplemented!(),
+                                    }
+                                },
+
+                                _ => unimplemented!(),
+                            };
+
+                            self.build_assign(var, &*rhs, &mut block);
+                        },
+
+                        ast::Expression::BinaryOp { op, lhs, rhs } => {
+                            let local = self.add_local(None);
+                            let res = self.build_expr(rhs, &mut block);
+                            block.ops.push(Op::Mov(local, res));
+                            match op {
+                                ast::BinaryOp::LShift => {
+                                    block.ops.push(Op::Put(local));
+                                },
+                                _ => unimplemented!(),
+                            };
+                        },
+
+                        _ => unimplemented!(),
+                    }
                 },
+
+                _ => unimplemented!(),
             }
         }
 
         block
     }
 
-    fn build_expr(&mut self, expr: &ast::Expr, block: &mut Block) -> Expr {
+    fn build_assign(&mut self, var: &str, expr: &ast::Expression, block: &mut Block) {
+        let var_id = self.lookup_var(var).unwrap();
+        let asg_expr = self.build_expr(expr, block);
+        block.ops.push(Op::Mov(var_id, asg_expr));
+    }
+
+    fn build_expr(&mut self, expr: &ast::Expression, block: &mut Block) -> Expr {
         match expr {
-            ast::Expr::Term(t) => self.build_term(t),
-            ast::Expr::Add(lhs, rhs) => {
+            ast::Expression::Base { unary, term, follow } => {
+                assert!(unary.len() == 0);
+                assert!(follow.len() == 0);
+                self.build_term(term)
+            },
+
+            ast::Expression::BinaryOp { op, lhs, rhs } => {
                 let lhs_expr = self.build_expr(lhs, block);
                 let rhs_expr = self.build_expr(rhs, block);
                 let local = self.add_local(None);
-                block.ops.push(Op::Add(local, lhs_expr, rhs_expr));
+
+                match op {
+                    ast::BinaryOp::Add => {
+                        block.ops.push(Op::Add(local, lhs_expr, rhs_expr));
+                    },
+
+                    _ => unimplemented!(),
+                }
+
                 Expr::Place(Place::Local(local))
-            }
+            },
+
+            _ => unimplemented!(),
         }
     }
 
     fn build_term(&self, term: &ast::Term) -> Expr {
         match term {
-            ast::Term::Literal(x) => Expr::Literal(Literal::Num(*x as f32)),
-            ast::Term::Ref(var_name) => {
+            ast::Term::Int(x) => Expr::Literal(Literal::Num(*x as f32)),
+            ast::Term::Float(x) => Expr::Literal(Literal::Num(*x)),
+            ast::Term::Ident(var_name) => {
                 let var_id = self.lookup_var(var_name).unwrap();
                 Expr::Place(Place::Local(var_id))
             },
+            _ => unimplemented!(),
         }
     }
 
-    fn lookup_var(&self, var: &str) -> Option<u32> {
+    fn lookup_var(&self, var: &str) -> Option<LocalId> {
         self.vars.get(var).map(|id| *id)
     }
 }
 
-type LocalID = u32;
+newtype_index!(LocalId);
 
 #[derive(Debug)]
 struct Local {
-    id: u32,
+    id: LocalId,
     name: Option<String>,
 }
 
 #[derive(Debug)]
 struct Proc {
-    locals: Vec<Local>,
-    vars: HashMap<String, u32>,
+    locals: IndexVec<LocalId, Local>,
+    vars: HashMap<String, LocalId>,
 
     blocks: Vec<Block>,
 }
@@ -160,9 +209,9 @@ impl Block {
 
 #[derive(Debug)]
 enum Op {
-    Mov(u32, Expr),
-    Put(u32),
-    Add(u32, Expr, Expr),
+    Mov(LocalId, Expr),
+    Put(LocalId),
+    Add(LocalId, Expr, Expr),
 }
 
 #[derive(Debug)]
@@ -181,13 +230,14 @@ enum Expr {
 
 #[derive(Debug)]
 enum Place {
-    Local(u32),
+    Local(LocalId),
     Global(u32),
 }
 
 #[derive(Debug)]
 enum Terminator {
     Return,
+    Jump(u32),
     Switch {
         discriminant: Place,
         branches: Vec<(Literal, u32)>,
@@ -197,7 +247,7 @@ enum Terminator {
 
 #[derive(Debug)]
 struct ProcEmit {
-    local_allocs: Vec<inkwell::values::PointerValue>,
+    local_allocs: IndexVec<LocalId, inkwell::values::PointerValue>,
     blocks: Vec<inkwell::basic_block::BasicBlock>,
     func: inkwell::values::FunctionValue,
 }
@@ -205,7 +255,7 @@ struct ProcEmit {
 impl ProcEmit {
     fn new(func: inkwell::values::FunctionValue) -> Self {
         Self {
-            local_allocs: Vec::new(),
+            local_allocs: IndexVec::new(),
             blocks: Vec::new(),
             func: func,
         }
@@ -251,7 +301,7 @@ impl Builder {
     }
 
     fn emit_proc(&self, proc: &Proc) {
-        let func_type = self.ctx.void_type().fn_type(&[], false);
+        let func_type = self.ctx.f32_type().fn_type(&[], false);
         let func = self.module.add_function("main", func_type, None);
         let mut proc_emit = ProcEmit::new(func);
         let entry_block = self.emit_entry_block(proc, &mut proc_emit);
@@ -292,7 +342,7 @@ impl Builder {
             Expr::Place(place) => {
                 match place {
                     Place::Local(id) => {
-                        self.builder.build_load(proc_emit.local_allocs[*id as usize], "").into()
+                        self.builder.build_load(proc_emit.local_allocs[*id], "").into()
                     },
                     Place::Global(_) => panic!("todo"),
                 }
@@ -305,26 +355,37 @@ impl Builder {
             match op {
                 Op::Mov(id, expr) => {
                     let val = self.load_expr(expr, proc_emit);
-                    self.builder.build_store(proc_emit.local_allocs[*id as usize], val);
+                    self.builder.build_store(proc_emit.local_allocs[*id], val);
                 },
 
                 Op::Put(id) => {
                     let print_type = self.ctx.void_type().fn_type(&[self.ctx.f32_type().into()], false);
-                    //let print_func = self.module.get_or_add_function("dm_print_num", print_type);
                     let print_func = self.module.add_function("dm_print_num", print_type, None);
-                    let val = self.builder.build_load(proc_emit.local_allocs[*id as usize], "put");
+                    let val = self.builder.build_load(proc_emit.local_allocs[*id], "put");
                     self.builder.build_call(print_func, &mut [val], "put");
                 },
                 Op::Add(id, lhs, rhs) => {
                     let lhs_ref = self.load_expr(lhs, proc_emit).into_float_value();
                     let rhs_ref = self.load_expr(rhs, proc_emit).into_float_value();
                     let val = self.builder.build_float_add(lhs_ref, rhs_ref, "sum");
-                    self.builder.build_store(proc_emit.local_allocs[*id as usize], val);
+                    self.builder.build_store(proc_emit.local_allocs[*id], val);
                 }
             }
         }
 
-        self.builder.build_return(None);
+        match &block.terminator {
+            Terminator::Return => {
+                self.builder.build_return(Some(&proc_emit.local_allocs[LocalId::new(0)]));
+            },
+
+            Terminator::Jump(id) => {
+                self.builder.build_unconditional_branch(&proc_emit.blocks[*id as usize]);
+            },
+
+            Terminator::Switch { discriminant, branches, default } => {
+                unimplemented!();
+            },
+        }
     }
 }
 
