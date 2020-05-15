e329e8fefc526de2d5b147ab0f37825bd96ac644
Colm Ó hIcí <colmohici@gmail.com>
2020-05-15 04:02:14 +0200

    tweaked stringtable, made serializable, runtime init takes ser stringtable

--- src/runtime.rs
+++ src/runtime.rs
@@ -8,9 +8,10 @@ pub struct Runtime {
 
 #[expose_c_stubs(rt_runtime)]
 impl Runtime {
-    pub fn new() -> Self {
+    pub fn init(init_st_bytes: *const u8, init_st_n: i64) -> Self {
+        let init_st = unsafe { std::slice::from_raw_parts(init_st_bytes, init_st_n as usize) };
         Runtime {
-            string_table: string_table::StringTable::new(),
+            string_table: string_table::StringTable::deserialize(init_st),
         }
     }
 }
--- src/string_table.rs
+++ src/string_table.rs
@@ -1,6 +1,7 @@
 use std::cell::RefCell;
 use std::borrow::Cow;
 use std::collections::HashMap;
+use bincode;
 
 // THOUGHTS
 // 64 bits is maybe too big. for the sake of not overthinking stuff, I've left it as is, but it
@@ -55,6 +56,33 @@ impl StringTable {
 
         id
     }
+
+    pub fn serialize(&self) -> Vec<u8> {
+        let flat: Vec<(&u64, &String)> = self.strings.iter().collect();
+        bincode::serialize(&flat).unwrap()
+    }
+
+    pub fn deserialize(buf: &[u8]) -> StringTable {
+        let flat: Vec<(u64, String)> = bincode::deserialize(buf).unwrap();
+
+        let mut strings = HashMap::new();
+        let mut ids = HashMap::new();
+        let mut next_id = 0;
+
+        for (id, s) in flat {
+            strings.insert(id, s.clone());
+            ids.insert(s, id);
+            if id + 1 > next_id {
+                next_id = id + 1;
+            }
+        }
+
+        StringTable {
+            strings: strings,
+            ids: ids,
+            next_id: next_id,
+        }
+    }
 }
 
 #[cfg(test)]
@@ -72,11 +100,27 @@ mod tests {
     #[test]
     fn dedup() {
         let mut table = StringTable::new();
-        let id1 = table.put("Hello".into());
-        let id2 = table.put("Hello".into());
+        let id1 = table.put("Hello");
+        let id2 = table.put("Hello");
         assert_eq!(id1, id2);
 
-        let id3 = table.put("hello".into());
+        let id3 = table.put("hello");
         assert_ne!(id1, id3);
+    }
+
+    #[test]
+    fn serde() {
+        let mut src_table = StringTable::new();
+        src_table.put("hello");
+        src_table.put("world");
+
+        let buf = src_table.serialize();
+        let dest_table = StringTable::deserialize(&buf);
+
+        println!("{:?}", dest_table);
+
+        assert_eq!(src_table.strings, dest_table.strings);
+        assert_eq!(src_table.ids, dest_table.ids);
+        assert_eq!(src_table.next_id, dest_table.next_id);
     }
 }
