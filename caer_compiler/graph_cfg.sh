264af68f3dcf1bef3d9c9ef1de6097606d1a6589
Colm Ó hIcí <git@ohici.ie>
2020-08-10 22:40:20 +0000

    fix graph_cfg

--- caer_compiler/graph_cfg.sh
+++ caer_compiler/graph_cfg.sh
@@ -6,7 +6,7 @@ mkdir -p dbgout/dot/llvm_unopt
 mkdir -p dbgout/dot/llvm_opt
 
 cd dbgout/dot/llvm_unopt
-llvm-as-10 < ../../llvm/unopt.ll | opt-10 -analyze -dot-cfg
+llvm-as-10 < ../../../out/unopt.ll | opt-10 -analyze -dot-cfg
 cd ../llvm_opt
-llvm-as-10 < ../../llvm/opt.ll | opt-10 -analyze -dot-cfg
+llvm-as-10 < ../../../out/opt.ll | opt-10 -analyze -dot-cfg
 cd ..
